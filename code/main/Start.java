4e3b830c8104590dd21d64ac7c20bf6e96629013
Voragun Supawong <voragun2543@gmail.com>
2022-09-19 18:02:24 +0700

    setup dependency and basic web service

---
+++ code/main/Start.java
@@ -0,0 +1,28 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+class Start {
+    public static void main(String[] data) {
+        ApplicationContext context;
+        context = SpringApplication.run(Setup.class);
+    }
+}
+
+// class Setup is use for create bean
+@SpringBootApplication
+class Setup {
+    
+}
+
+@RestController
+class Sample {
+    @RequestMapping("/check")
+    String show() {
+        return "I love you";
+    }
+}
