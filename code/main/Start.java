559e48329bdb426f02fd00f65a29e8cca2218434
Voragun Supawong <voragun2543@gmail.com>
2022-09-20 17:03:54 +0700

    Parameter Injection

--- code/main/Start.java
+++ code/main/Start.java
@@ -3,6 +3,7 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,24 @@ class Start {
     public static void main(String[] data) {
         ApplicationContext context;
         context = SpringApplication.run(Setup.class);
+        String s = context.getBean(String.class);
+        System.out.println(s);
+        SpringApplication.exit(context);
     }
 }
 
 // class Setup is use for create bean
 @SpringBootApplication
 class Setup {
+    @Bean
+    String city(Integer k) {
+        return "London" + k;
+    }
     
+    @Bean
+    Integer code() {
+        return 806;
+    }
 }
 
 @RestController
